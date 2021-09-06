5f87e5bae99dba695de4dd41ddaca51d74b6d11a
Svebbeee <sandlandsverre@gmail.com>
2021-09-06 16:25:46 +0200

    lol

---
+++ src/test/java/no/kristiania/http/HttpClientTest.java
@@ -0,0 +1,11 @@
+package no.kristiania.http;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HttpClientTest {
+    @Test
+    void DummyTest(){
+        assertEquals(42, 6*7);
+    }
+}
